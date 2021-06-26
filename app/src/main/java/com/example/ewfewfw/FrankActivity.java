1ce3f68802145c4bee2009c03475a7355bd38a26
blobthebuilder <68312664+blobthebuilder@users.noreply.github.com>
2021-06-25 20:31:47 -0700

    solved the world's problems

--- app/src/main/java/com/example/ewfewfw/FrankActivity.java
+++ app/src/main/java/com/example/ewfewfw/FrankActivity.java
@@ -2,13 +2,30 @@ package com.example.ewfewfw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FrankActivity extends AppCompatActivity {
 
+    Button button;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frank);
+        textView = findViewById(R.id.textView2);
+        button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Frank is gay");
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
