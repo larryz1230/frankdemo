0157b9538758165715dccc2a226cc222fb389535
larryz1230 <larryzhi1230@gmail.com>
2021-06-25 20:11:31 -0700

    first commit

---
+++ app/src/main/java/com/example/ewfewfw/FrankActivity.java
@@ -0,0 +1,14 @@
+package com.example.ewfewfw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FrankActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frank);
+    }
+}
---
+++ app/src/main/java/com/example/ewfewfw/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.ewfewfw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Frank is gay");
+                Intent intent = new Intent(view.getContext(), FrankActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
